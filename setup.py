5b7d52c9ef416f5eb592c99d51d9411b203d5b93
Sambit Ghosh <sambit1089@gmail.com>
2025-03-17 23:06:50 +0530

    folder added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'legal_advisor Chatbot',
+    version= '0.0.0',
+    author= 'Sambit',
+    packages= find_packages(),
+    install_requires = []
+
+)
